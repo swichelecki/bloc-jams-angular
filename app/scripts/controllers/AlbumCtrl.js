c6e444107076576d1c0ff5aa15d3d2cf5d5e015c
Steve Wichelecki <swichelecki@gmail.com>
2016-11-19 16:30:46 -0600

    Add checkpoint 7 work

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,31 +1,14 @@
 (function() {
-    function AlbumCtrl(Fixtures) {
+    function AlbumCtrl(Fixtures, SongPlayer) {
         
         this.albumData = Fixtures.getAlbum();
-        //console.log(this.albumData);
-        //this.albumData = {};
-        
-     /*   this.albumTitle = albumData.title;
-        //console.log(albumData.title);
-        this.albumArtist = albumData.artist;
-        this.albumLabel = albumData.label;
-        this.albumYear = albumData.year;
-        this.albumCover = albumData.albumArtUrl; */
-        
-        //this.albumData = [];
-        //this.songs = [];
-        
-          //  for (var i = 0; i < this.albumData.songs.length; i++) {
-        
-              // this.songs.push(this.albumData.songs[i]);  
-                //this.albumData.push(albumPicasso.songs[i]);    
-        
-          //  } 
+        this.songPlayer = SongPlayer;
+    
         
     }
     
     angular
         .module('blocJams')
-        .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
+        .controller('AlbumCtrl', ['Fixtures', 'SongPlayer', AlbumCtrl]);
     
 })();
